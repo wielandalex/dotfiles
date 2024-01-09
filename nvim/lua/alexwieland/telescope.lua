87b29daad0b11f62ec0199d3acb1ed1a91bfbb11
Alex Wieland <alexanderwieland0@gmail.com>
2024-01-09 08:37:03 +0100

    Update telescope keybinds

--- nvim/lua/alexwieland/telescope.lua
+++ nvim/lua/alexwieland/telescope.lua
@@ -11,10 +11,11 @@ require("telescope").setup({
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "Find recently opened files" })
-vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy search in current buffer" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[F]ind [G]it Files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind with live [G]rep" })
+vim.keymap.set("n", "<leader>s?", builtin.oldfiles, { desc = "[S]earch recently opened files" })
+vim.keymap.set("n", "<leader>s/", builtin.current_buffer_fuzzy_find, { desc = "[S]earch in current buffer" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch [G]it Files" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch with live [G]rep" })
+vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
