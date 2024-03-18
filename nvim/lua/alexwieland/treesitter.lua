741b06c5f27c2a79d1ae45bddf7e3d4013c8cb64
Alex Wieland <alexanderwieland0@gmail.com>
2024-03-18 08:19:55 +0100

    nvim: disable treesitter for cshtml

--- nvim/lua/alexwieland/treesitter.lua
+++ nvim/lua/alexwieland/treesitter.lua
@@ -2,6 +2,16 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "lua", "go", "c_sharp", "javascript", "typescript", "css", "html" },
     auto_install = true,
-    highlight = { enable = true },
+    highlight = {
+        enable = true,
+        disable = function(lang, buf)
+            local ext = vim.fn.expand("%:e")
+            if ext == "cshtml" then
+                return true
+            end
+
+            return false
+        end,
+    },
     indent = { enable = true },
 })
