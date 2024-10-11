2bba2700f367d4dd1de3240fc4502e926dd6e95e
Alex Wieland <alexanderwieland0@gmail.com>
2024-10-11 10:44:51 +0200

    nvim: add autopairs

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
+
+    {
+        "windwp/nvim-ts-autotag",
+        config = true,
+    },
+}
