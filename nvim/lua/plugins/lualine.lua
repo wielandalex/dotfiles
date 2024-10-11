55c494b2c12f58623281ba54408fb5e399cc4156
Alex Wieland <alexanderwieland0@gmail.com>
2024-10-11 10:31:21 +0200

    nvim: add lualine dev icons dependency

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     opts = {
         options = {
             component_separators = {},
