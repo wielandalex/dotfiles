dec319b8aa0b50e4c0674f419533dca973b8e7d7
Alex Wieland <alexanderwieland0@gmail.com>
2024-10-11 10:30:19 +0200

    nvim: colorscheme changes

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -2,7 +2,9 @@ return {
     "rose-pine/neovim",
     lazy = true,
     name = "rose-pine",
+    priority = 1000,
     opts = {
+        variant = "moon",
         styles = {
             italic = false,
         },
